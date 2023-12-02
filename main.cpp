7c0a538a7d7c1e6f42ddeb4f6ff089ca8b65e557
Sebastian Vallejos <sebastian.vallejos2001@alumnos.ubiobio.cl>
2023-12-01 23:46:33 -0300

    wena

--- main.cpp
+++ main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
-int main(int, char**){
-    std::cout << "Hello, from fotos!\n";
+int main() {
+    // Especifica la ruta completa de la imagen
+    cv::Mat imagen = cv::imread("/ruta/completa/a/imagen.jpg");
+
+    // Verifica si la imagen se cargó correctamente
+    if (imagen.empty()) {
+        std::cerr << "Error al cargar la imagen" << std::endl;
+        return -1;
+    }
+
+    // Muestra la imagen en una ventana
+    cv::imshow("Imagen", imagen);
+
+    // Espera hasta que se presiona una tecla
+    cv::waitKey(0);
+
+    return 0;
 }
+
